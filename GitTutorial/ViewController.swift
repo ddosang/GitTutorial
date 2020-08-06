3dc4720368409ae1859414ccda9268f791e3a557
ddosang <218926@naver.com>
2020-08-07 02:09:43 +0900

    Add another print

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("공부하기귀찮다...")
         print("hello world!")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
